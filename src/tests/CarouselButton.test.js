7da3cdbd4051f81fb47d60a0dd1f93860f3f134c
Jaudat Syed <jsyed@ca.ibm.com@Jaudats-MacBook-Pro.local>
2019-08-06 22:21:07 -0400

    :wrench: Add Jest setup file

--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adaptor from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import CarousalButton from '../CarouselButton';
 
-configure({ adapter: new Adaptor() });
 
 describe('CarouselButton', () => {
   const text = 'Button text';
