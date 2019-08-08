95e9e50871f23128f489766d9111fba1f9af3d6b
Jaudat Syed <jsyed@ca.ibm.com@Jaudats-MacBook-Pro.local>
2019-08-08 00:25:51 -0400

    No need for parserOptions as we are now using the 'babel-eslint' parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   plugins: ['react'],
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parser: "babel-eslint",
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
+  // parserOptions: {
+  //   ecmaVersion: 6,
+  //   sourceType: 'module',
+  //   ecmaFeatures: {
+  //     jsx: true,
+  //   },
+  // },
   env: {
     node: true,
   },
