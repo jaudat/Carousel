c08b1c573d7ac20eba34db98cc9429cc3144835d
Jaudat Syed <jsyed@ca.ibm.com@Jaudats-MacBook-Pro.local>
2019-08-08 00:26:39 -0400

    removed commented out code

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,6 @@ module.exports = {
   plugins: ['react'],
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parser: "babel-eslint",
-  // parserOptions: {
-  //   ecmaVersion: 6,
-  //   sourceType: 'module',
-  //   ecmaFeatures: {
-  //     jsx: true,
-  //   },
-  // },
   env: {
     node: true,
   },
