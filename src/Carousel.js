b20e4950b165fa50b70aeea83e3726a292b8b3dd
Jaudat Syed <jsyed@ca.ibm.com@Jaudats-MacBook-Pro.local>
2019-08-12 22:26:41 -0400

    :sparkles: Add prop for extending the <img> component

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,11 +5,13 @@ import CarouselSlide from './CarouselSlide';
 
 class Carousel extends React.PureComponent {
   static propTypes = {
+    defaultImg: CarouselSlide.propTypes.Img,
     defaultImgHeight: CarouselSlide.propTypes.imgHeight,
     slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes)).isRequired,
   }
 
   static defaultProps = {
+    defaultImg: CarouselSlide.defaultProps.Img,
     defaultImgHeight: CarouselSlide.defaultProps.imgHeight,
   }
 
@@ -32,10 +34,14 @@ class Carousel extends React.PureComponent {
   }
 
   render() {
-    const { defaultImgHeight, slides, ...rest } = this.props;
+    const { defaultImg, defaultImgHeight, slides, ...rest } = this.props;
     return (
       <div {...rest}>
-        <CarouselSlide imgHeight={defaultImgHeight} {...slides[this.state.slideIndex]} />
+        <CarouselSlide
+          Img={defaultImg}
+          imgHeight={defaultImgHeight}
+          {...slides[this.state.slideIndex]}
+        />
         <CarouselButton data-action='prev' onClick={this.handlePrevClick}>Prev</CarouselButton>
         <CarouselButton data-action='next' onClick={this.handleNextClick}>Next</CarouselButton>
       </div>
--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import Carousel from '../Carousel';
 import CarouselButton from '../CarouselButton';
 import CarouselSlide from '../CarouselSlide';
+import { isRegExp } from 'util';
 
 describe('Carousel', () => {
   let wrapper;
@@ -112,6 +113,23 @@ describe('Carousel', () => {
       ...CarouselSlide.defaultProps,
       ...slides[1],
     });
+  });
+
+  it('passes defaultImg and defaultImgHeight to the CarouselSlide', () => {
+    const defaultImg = () => 'test';
+    const defaultImgHeight = 1234;
+    wrapper.setProps({ defaultImg, defaultImgHeight });
+    expect(wrapper.find(CarouselSlide).prop('Img')).toBe(defaultImg);
+    expect(wrapper.find(CarouselSlide).prop('imgHeight')).toBe(defaultImgHeight);
+  });
+
+  it('allows individual slides to override Img and imgHeight', () => {
+    const Img = () => 'test';
+    const imgHeight = 1234;
+
+    wrapper.setProps({ slides: [{ ...slides[0], Img, imgHeight }] })
+    expect(wrapper.find(CarouselSlide).prop('Img')).toBe(Img);
+    expect(wrapper.find(CarouselSlide).prop('imgHeight')).toBe(imgHeight);
   });
 
 });
