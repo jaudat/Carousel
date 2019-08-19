07761141a20ec847823722284f1c45842e6b0752
Jaudat Syed <jsyed@ca.ibm.com@jaudats-mbp.spa.can.ibm.com>
2019-08-19 17:52:36 -0400

    :sparkles: Add auto-advance support to Carousel. also fixed test errors where we used shallow testing instead of mounted for Component HOC

--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -25,27 +25,37 @@ describe('Carousel', () => {
 
 
   describe('component with HOC', () => {
-    // Tests agains't Carousel will go here
-    let wrapper;
+    let mounted;
 
     beforeEach(() => {
-      wrapper = shallow(<Carousel slides={slides} />);
+      mounted = mount(<Carousel slides={slides} />);
+    });
+
+    it('passes `slides` down to the core component', () => {
+      expect(mounted.find(CoreCarousel).prop('slides')).toBe(slides);
     });
 
     it('sets slideIndex={0} on the core component', () => {
-      expect(wrapper.find(CoreCarousel).prop('slideIndex')).toBe(0);
-    })
+      expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(0);
+    });
 
-    it('passes `slides` down to the coreComponent', () => {
-      expect(wrapper.find(CoreCarousel).prop('slides')).toBe(slides);
-    }),
+    it('allows `slideIndex` to be controlled', () => {
+      mounted = mount(<Carousel slides={slides} slideIndex={1} />);
+      expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(1);
+      mounted.setProps({ slideIndex: 0 });
+      expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(0);
+    });
 
-      it('allows `slideIndex` to be controlled', () => {
-        const mounted = mount(<Carousel slides={slides} slideIndex={1} />);
-        expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(1);
-        mounted.setProps({ slideIndex: 0 });
-        expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(0);
-      });
+    it('advances the slide after `autoAdvanceDelay` elapses', () => {
+      jest.useFakeTimers();
+      const autoAdvanceDelay = 10e3;
+      mounted = mount(
+        <Carousel slides={slides} autoAdvanceDelay={autoAdvanceDelay} />
+      );
+      jest.advanceTimersByTime(autoAdvanceDelay);
+      mounted.update(); //(1)
+      expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(1);
+    });
   });
 
   describe('core component', () => {
@@ -164,6 +174,4 @@ describe('Carousel', () => {
       expect(wrapper.find(CarouselSlide).prop('imgHeight')).toBe(imgHeight);
     });
   });
-
-
 });
